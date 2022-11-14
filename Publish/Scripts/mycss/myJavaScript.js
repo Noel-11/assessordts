9b703432928b2a20c5f5527c4c2c47a55b758f16
Noel-11 <72383121+Noel-11@users.noreply.github.com>
2022-11-14 15:27:48 +0800

    initial upload

---
+++ Publish/Scripts/mycss/myJavaScript.js
@@ -0,0 +1,12 @@
+﻿
+
+$(function () {
+
+    $('.gridviewPager').closest('tr').find('table tbody tr').
+    prepend('<td><a href="javascript:__doPostBack(' +
+    "'_gv'" + ',' + "'Page$First'" + ')">First</a></td>');
+
+    $('.gridviewPager').closest("tr").find("table tbody tr")
+    .append('<td><a href="javascript:__doPostBack(' +
+    "'_gv'" + ',' + "'Page$Last'" + ')">Last</a></td>');
+})
